31636d58206c4af369ef426f72fe876931737b95
Md Imran <xsmartarman123@gmail.com>
2025-11-25 17:30:25 +0530

    Disable lightningcss for Render deployment

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  css: {
+    transformer: "postcss"   // 🔥 LightningCSS disable (main fix)
+  },
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 });
